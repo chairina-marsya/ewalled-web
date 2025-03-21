196405162b257947f4fa855745b722581593668c
Chairina <>
2025-03-21 10:43:10 +0700

    chore:learn basic java

---
+++ basic-java/Main.java
@@ -0,0 +1,10 @@
+public class Main {
+    public static void main(String[] args) {
+        String name = "BSI";
+        int x = 9;
+        int y = 5;
+        int sum = x + y;
+        System.out.println("Hello, " + name);
+        System.out.println("jumlah: " + sum);
+    }
+}
